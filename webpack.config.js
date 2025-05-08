87e49a0ef140b2043ed8f30ae2b716068437149d
mubanga-1 <mubanga.s10@gmail.com>
2025-05-08 15:49:04 -0700

    Install and Add html-webpack-plugin to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 module.exports = {
     mode: "development",
@@ -7,5 +9,10 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    }
-}
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+};  
