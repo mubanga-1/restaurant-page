fc82c8be22ffe0853144071eb4c8e8970d85034c
mubanga-1 <mubanga.s10@gmail.com>
2025-05-13 07:08:33 -0700

    Export function from page files

--- src/pages.js
+++ src/pages.js
@@ -1,6 +1,11 @@
 function clearScreen() {
     const content = document.querySelectorAll("#content > *");
     for (let i = 0; i < content.length; i++) {
-        document.removeChild(content[i]);
+        document.querySelector("#content").removeChild(content[i]);
     }
 }
+
+export { clearScreen };
+export { createHome } from "./home.js";
+export { createMenu } from "./menu.js";
+export { createContact } from "./contact.js";
