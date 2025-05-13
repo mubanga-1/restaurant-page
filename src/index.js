f36412933af475c602766a2a749b3bfdd1d2b243
mubanga-1 <mubanga.s10@gmail.com>
2025-05-13 03:00:23 -0700

    Implement visual indication of tab switching
    Implemented visually the notion of switching between different page nav
    options in tabbed format.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import "./styles.css";  
 import {createHome} from "./home.js";
 
+function highlight(element) {
+    element.style.backgroundColor = "#f1f2f3";
+    element.style.color = "#A67B5B";
+}
 
-const homeBtn = document.querySelector("#home");
+function unhighlight(element) {
+    element.style.backgroundColor = "";
+    element.style.color = "";
+}
 
-homeBtn.addEventListener("click", () => {
+// Display the homepage's content when page loads for first time
+document.addEventListener("DOMContentLoaded", () => {
+    highlight(document.querySelector("#home"));
     createHome();
-})
+});
+
+// Change between what information to display based on which navigation button is clicked
+
+const navBtnContainer = document.querySelector(".links");
+const buttons = document.querySelectorAll(".links button");
+
+navBtnContainer.addEventListener("click", (event) => {
+    let target = event.target;
+    
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].id === target.id) {
+            highlight(buttons[i]);
+        } else {
+            unhighlight(buttons[i]);
+        }
+    }
+});
